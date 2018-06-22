82915eeb6023f61fe0b4138d6183d8e6b2b3bb74
paul <ztt1995120@gmail.com>
2018-06-22 21:47:58 +0800

    1.6 丰富冥想Aty，有了基本的布局，待计时功能

--- app/src/main/java/com/paul/t29ideagarden2/atys/MeditationActivity.java
+++ app/src/main/java/com/paul/t29ideagarden2/atys/MeditationActivity.java
@@ -1,6 +1,19 @@
 package com.paul.t29ideagarden2.atys;
 
 import android.support.v7.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.paul.t29ideagarden2.R;
 
 /**
  * Created by paul on 2018/6/22
@@ -9,4 +22,68 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class MeditationActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private List<String> mDatas;
+    private HomeAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meditation);
+
+        initData();
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mAdapter = new HomeAdapter());
+
+    }
+
+    protected void initData()
+    {
+        mDatas = new ArrayList<String>();
+        for (int i = 'A'; i < 'z'; i++)
+        {
+            mDatas.add("" + (char) i);
+        }
+    }
+
+    class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder>
+    {
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(
+                    MeditationActivity.this).inflate(R.layout.item_home, parent,
+                    false));
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position)
+        {
+            holder.tv.setText(mDatas.get(position));
+        }
+
+        @Override
+        public int getItemCount()
+        {
+            return mDatas.size();
+        }
+
+        class MyViewHolder extends ViewHolder
+        {
+
+            TextView tv;
+
+            public MyViewHolder(View view)
+            {
+                super(view);
+                tv = (TextView) view.findViewById(R.id.id_num);
+            }
+        }
+    }
+
 }
+
