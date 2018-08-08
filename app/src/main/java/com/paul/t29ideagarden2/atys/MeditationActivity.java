281257a495b40327ed5059e71dd80710a3c27266
paul <ztt1995120@gmail.com>
2018-08-08 23:19:47 +0800

    1.12 添加了用户头像修改功能（解析uri得到filepath，用数据库保存绝对路径）

--- app/src/main/java/com/paul/t29ideagarden2/atys/MeditationActivity.java
+++ app/src/main/java/com/paul/t29ideagarden2/atys/MeditationActivity.java
@@ -1,10 +1,15 @@
 package com.paul.t29ideagarden2.atys;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import java.util.ArrayList;
@@ -19,6 +24,7 @@ import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,6 +37,8 @@ import com.paul.t29ideagarden2.helper.MonkDatabaseHelper;
 import com.paul.t29ideagarden2.presenter.MeditationPresenter;
 import com.paul.t29ideagarden2.view.IMonkMeditationView;
 
+import static com.paul.t29ideagarden2.util.Constants.ACTIVITY_REQUEST_FOR_CHANGE_USER_IMG;
+
 /**
  * Created by paul on 2018/6/22
  * last modified at 8:48 PM.
@@ -39,6 +47,7 @@ import com.paul.t29ideagarden2.view.IMonkMeditationView;
 
 public class MeditationActivity extends AppCompatActivity implements IMonkMeditationView{
     private RecyclerView mRecyclerView;
+    private ImageView headImg;
     private FloatingActionButton fab;
     private ProgressBar mProgressBar;
     private List<String> mDatas;
@@ -60,10 +69,32 @@ public class MeditationActivity extends AppCompatActivity implements IMonkMedita
 
 
     }
+
+    protected void initData()
+    {
+        monkDatabaseHelper = new MonkDatabaseHelper(this,"MonkRecord.db",null,1);
+        mDatas = new ArrayList<String>();
+        mDatas.addAll(Arrays.asList(FlowerCard.recyclerViewStrings));
+        insertDatabaseData();
+        mMonk = getMonk();
+    }
+
     void initViews(){
         mRecyclerView = findViewById(R.id.recyclerView);
         mRecyclerView.setLayoutManager(new GridLayoutManager(this,1));
         mRecyclerView.setAdapter(mAdapter = new HomeAdapter());
+        headImg = findViewById(R.id.user_profile);
+        if (getUserProfile(mMonk.getImgPath())!= null){
+            headImg.setImageBitmap(getUserProfile(mMonk.getImgPath()));
+        }
+        headImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pickPicIntent = new Intent("android.intent.action.PICK");
+                pickPicIntent.setType("image/*");
+                startActivityForResult(pickPicIntent,ACTIVITY_REQUEST_FOR_CHANGE_USER_IMG);
+            }
+        });
         mProgressBar = findViewById(R.id.progressBar);
         fab = findViewById(R.id.floatingActionButton);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -87,13 +118,14 @@ public class MeditationActivity extends AppCompatActivity implements IMonkMedita
         };
     }
 
-    protected void initData()
-    {
-        monkDatabaseHelper = new MonkDatabaseHelper(this,"MonkRecord.db",null,1);
-        mDatas = new ArrayList<String>();
-        mDatas.addAll(Arrays.asList(FlowerCard.recyclerViewStrings));
-        insertDatabaseData();
-        mMonk = getMonk();
+    Bitmap getUserProfile(String filePath){
+        Bitmap userProfile = BitmapFactory.decodeFile(filePath);
+        if (userProfile != null){
+            return userProfile;
+        }else {
+            return null;
+        }
+
     }
     @Override
     public Handler getHandler(){
@@ -146,6 +178,7 @@ public class MeditationActivity extends AppCompatActivity implements IMonkMedita
     private void insertDatabaseData(){
         SQLiteDatabase db = monkDatabaseHelper.getWritableDatabase();
         Cursor cursor = db.query("Monk",null,null,null,null,null,null);
+        //tt: only find no result, we execute db.insert();
         if (!cursor.moveToFirst()) {
             ContentValues cv = new ContentValues();
             cv.put("monk_name", "Paul");
@@ -155,6 +188,25 @@ public class MeditationActivity extends AppCompatActivity implements IMonkMedita
             db.insert("Monk", null, cv);
         }
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        SQLiteDatabase db = monkDatabaseHelper.getWritableDatabase();
+        if(requestCode == ACTIVITY_REQUEST_FOR_CHANGE_USER_IMG && resultCode == RESULT_OK && data != null){
+            Uri selectedImg = data.getData();
+            String [] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImg,filePathColumn,null,null,null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            Toast.makeText(this,"find Img at" + picturePath,Toast.LENGTH_SHORT).show();
+            ContentValues cv = new ContentValues();
+            cv.put("monk_img_path",picturePath);
+            db.update("Monk",cv,"monk_name = ?",new String[]{"Paul"});
+        }
     }
 
     class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder>
