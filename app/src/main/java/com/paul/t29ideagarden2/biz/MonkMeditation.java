8bdc7dbc61bc1d6f4e544d598036df84bbe4a3d3
paul <ztt1995120@gmail.com>
2018-09-22 21:40:30 +0800

    1.21 解决1。19的bug：现在用户多次连续点击不会真的重开计时线程 仍存在的bug：波浪到99就不到100，我希望到满了后自动停止这个该死的waveProgress动画

--- app/src/main/java/com/paul/t29ideagarden2/biz/MonkMeditation.java
+++ app/src/main/java/com/paul/t29ideagarden2/biz/MonkMeditation.java
@@ -2,6 +2,7 @@ package com.paul.t29ideagarden2.biz;
 
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 
 import com.paul.t29ideagarden2.bean.Monk;
 
@@ -15,15 +16,32 @@ import static com.paul.t29ideagarden2.util.Constants.TIME_UP_LIMIT;
  */
 
 public class MonkMeditation implements IMonkMeditation {
-    @Override
-    public synchronized void meditation(final Monk monk, final OnMeditationFinishedListener listener, final Handler handler) {
-        new Thread(){
-            @Override
-            public void run() {
-                super.run();
+
+    private boolean countingFlag = false;
+
+    class CounterDownThread implements Runnable {
+
+        //thread need vars
+        Monk monk;
+        OnMeditationFinishedListener listener;
+        Handler handler;
+
+        //todo: (0922)?make ur params final!
+        CounterDownThread(Monk _monk, OnMeditationFinishedListener _listener, Handler _handler) {
+            this.monk = _monk;
+            this.listener = _listener;
+            this.handler = _handler;
+        }
+
+        @Override
+        public void run() {
+            synchronized (this) {
+                countingFlag = true;
+                //count down by for loop
                 int counterDown = TIME_UP_LIMIT;//tt:now it's 10 min
-                for(;counterDown > 0;counterDown --){
+                for(;counterDown > 0;counterDown--){
                     try{
+                        //send heart beat msg by for loop
                         Message message = handler.obtainMessage(1);
                         message.arg1 = counterDown;
                         Thread.sleep(1000);
@@ -32,14 +50,30 @@ public class MonkMeditation implements IMonkMeditation {
                         e.printStackTrace();
                     }
                 }
-                //tt: 顺利完成meditation
-                if (monk != null){
+                //finished, check monk still there?
+                if (monk != null) {
                     listener.meditationFinished();
-                }else{
-                //tt: 未正常完成
+                } else {
                     listener.meditationInterrupted();
                 }
+                countingFlag = false;
             }
-        }.start();
+        }
+    }
+
+    @Override
+    public void meditation(final Monk monk, final OnMeditationFinishedListener listener, final Handler handler) {
+        //if not counting now, we counting, or send msg to user that we are counting down, don't
+        //bother us counting! fuck users!
+        if (!countingFlag) {
+            CounterDownThread counterDownThread = new CounterDownThread(monk, listener, handler);
+            Thread countDownThreadWrapper = new Thread(counterDownThread);
+            countDownThreadWrapper.start();
+            countingFlag = true;
+        } else {
+            // thread is counting, don't bother!
+            // todo: (0922) notice user we won't start counting
+            Log.e("MonkMeditation","counting down, don't bother");
+        }
     }
 }
